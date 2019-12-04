2e1184d1cc557e8d6b479d5271f3ef45cba0def6
Milos Jovanovic <m.jovanovic@levi9.com>
2019-12-04 12:23:19 +0100

    Judge Thread skeleton

---
+++ db/test-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO `team` VALUES 
+(1,'Red team','https://rryr2ini4g.execute-api.us-east-1.amazonaws.com/dev',90.000,'aws','eu-west-1','ready'),
+(2,'Blue team','http://sls-wus2-dev-hack9-azure.azurewebsites.net/api',80.500,'azure','West US 2','ready'),
+(3,'Green team',NULL,70.000,'aws','eu-west-1','ready'),
+(4,'Orange team',NULL,60.000,'aws','eu-west-1','ready'),
+(5,'Yellow team',NULL,50.000,'aws','eu-west-1','ready'),
+(6,'Purple team',NULL,40.000,'aws','eu-west-1','ready'),
+(7,'Brown team',NULL,30.000,'aws','eu-west-1','ready');
+
+INSERT INTO `user` VALUES 
+(1,'Miloš Jovanović','m.jovanovic@levi9.com',1,'admin'),
+(2,'Dragan Gajić','d.gajic@levi9.com',2,'admin'),
+(3,'Nikola Milutinović','n.milutinovic@levi9.com',3,'admin'),
+(4,'Dragana Momčilović','d.momcilovic@levi9.com',4,'admin'),
+(5,'Mirjana Kolarov','mi.kolarov@levi9.com',4,'admin'),
+(6,'Dmytro Panin','d.panin@levi9.com',5,'admin'),
+(7,'Ionut Stanescu','i.stanescu@levi9.com',5,'admin');
